1f649b2c298e58b58be4b7ecaddd7590a9af497c
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2025-08-17 15:27:14 -0300

    Adicionando opção de executar a API como script python para debugging

--- madr/app.py
+++ madr/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from .routes import auth, authors, books
+from madr.routes import auth, authors, books
+import uvicorn
 
 app = FastAPI()
 
@@ -7,3 +8,6 @@ app = FastAPI()
 app.include_router(auth.router)
 app.include_router(authors.router)
 app.include_router(books.router)
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", reload=True)
