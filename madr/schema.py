446602e6bc8762efbdf848343fdf2fabdd87c2bb
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2025-08-26 22:19:49 -0300

    Adicionando tipo extra de ISBN e modificando schemas de livros
    Criando esquemas de livros com campos da tabela e BeforeValidator, junto
    de uma lista que vai ser usada para vincular autores já existentes a um
    livro
    
    Adicionando esquemas para autores

--- madr/schema.py
+++ madr/schema.py
@@ -1,6 +1,15 @@
+from datetime import date
 from typing import Annotated
 from uuid import UUID
-from pydantic import BaseModel, ConfigDict, EmailStr, Field, BeforeValidator
+
+from pydantic import (
+    BaseModel,
+    BeforeValidator,
+    ConfigDict,
+    EmailStr,
+    Field,
+)
+from pydantic_extra_types.isbn import ISBN
 
 from madr.core.security import hash_password
 from madr.utils.sanitization import sanitize_name
@@ -30,8 +39,47 @@ class UserSchema(UserBase):
     id: UUID
 
 
-class BookSchema(BaseModel):
-    isbn: str
+class BookBase(BaseModel):
+    isbn: ISBN
+    name: Annotated[str, BeforeValidator(sanitize_name)] = Field(alias="nome")
+    year: int = Field(alias="ano")
+    author_ids: list[int] = Field(alias="ids_romancistas", default=[])
+
+    model_config = ConfigDict(validate_by_name=True)
 
 
-class Author(BaseModel): ...
+class BookCreate(BookBase): ...
+
+
+class BookUpdate(BaseModel):
+    isbn: ISBN | None
+    name: Annotated[str | None, BeforeValidator(sanitize_name)] = Field(alias="nome")
+    year: int | None = Field(alias="ano")
+    author_ids: list[int] | None = Field(alias="ids_romancistas")
+
+    model_config = ConfigDict(validate_by_name=True)
+
+
+class BookSchema(BookBase):
+    id: int
+
+
+class AuthorBase(BaseModel):
+    name: str = Field(alias="nome")
+    nationality: str = Field(alias="nacionalidade")
+    birth_date: date = Field(alias="data-nascimento")
+
+    model_config = ConfigDict(validate_by_name=True)
+
+
+class AuthorSchema(AuthorBase):
+    id: int
+
+
+class AuthorCreate(AuthorBase): ...
+
+
+class AuthorUpdate(BaseModel):
+    name: str | None
+    nationality: str | None = Field(alias="nacionalidade")
+    birth_date: date | None = Field(alias="data-nascimento")
