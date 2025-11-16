b0cc0afe15c91c1e22eaa4f6b2c113dc41f08d2d
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2025-11-16 10:56:06 -0300

    Corrigindo não determinismo nos testes
    Resolve conflitos não esperados ao usar valores aleatórios nas colunas
    que devem ser únicas, usa sequências do factory boy no lugar
    
    Cria função auxiliar que converte números para algarismos romanos para
    debug dos objetos sem interferir nos validadores

--- tests/factories.py
+++ tests/factories.py
@@ -7,12 +7,33 @@ from madr.schema import AuthorCreate, BookCreate, UserCreate
 factory.Faker._DEFAULT_LOCALE = "pt_BR"
 
 
+def num_to_roman(n: int) -> str:
+    """
+    Fonte: https://www.geeksforgeeks.org/python/python-program-to-convert-integer-to-roman/
+    """
+    num = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+    sym = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+    i = 12
+
+    s = ""
+
+    while n:
+        div = n // num[i]
+        n %= num[i]
+
+        while div:
+            s += sym[i]
+            div -= 1
+        i -= 1
+    return s
+
+
 class UserCreateFactory(factory.Factory):
     class Meta:
         model = UserCreate
 
     username = factory.Faker("name")
-    email = factory.Faker("email")
+    email = factory.Sequence(lambda n: f"email{n}@test.com")
     password = factory.Faker("password", length=8)
 
 
@@ -21,7 +42,11 @@ class BookCreateFactory(factory.Factory):
         model = BookCreate
 
     isbn = factory.Faker("isbn13", separator="")
-    name = factory.Faker("catch_phrase")
+
+    @factory.sequence
+    def name(n):
+        return f"Livro {num_to_roman(n)}"
+
     year = factory.Faker("year")
 
 
@@ -29,6 +54,9 @@ class AuthorCreateFactory(factory.Factory):
     class Meta:
         model = AuthorCreate
 
-    name = factory.Faker("name")
+    @factory.sequence
+    def name(n):
+        return f"Autor {num_to_roman(n)}"
+
     birth_date = factory.Faker("date")
     nationality = factory.Faker("country")
